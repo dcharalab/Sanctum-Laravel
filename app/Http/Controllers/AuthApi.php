bfd813ab6c5ab46a09529afa5171f8f51d64536d
dcharalab <dcharalab@gmail.com>
2022-06-24 19:33:44 +0300

    initial commit

---
+++ app/Http/Controllers/AuthApi.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+
+class AuthApi extends Controller
+{
+    public function response($user)
+    {
+        $token = $user->createToken(str()->random(40))->plainTextToken;
+        return response()->json([
+            'user'=>$user,
+            'token'=>$token,
+            'token_type'=>'Bearer'
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|min:3',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:8|
+                        regex:/[a-z]/|
+                        regex:/[A-Z]/|
+                        regex:/[0-9]/|
+                        regex:/[@$!%*#?&]/|
+                        confirmed'
+        ]);
+
+        $user = User::create([
+            'name'=>ucwords($request->name),
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]);
+
+        return $this->response($user);
+    }
+
+    public function login(Request $request)
+    {
+        $cred = $request->validate([
+            'email'=>'required|email|exists:users',
+            'password'=>'required|min:8|
+                        regex:/[a-z]/|
+                        regex:/[A-Z]/|
+                        regex:/[0-9]/|
+                        regex:/[@$!%*#?&]/'
+        ]);
+
+        if(!Auth::attempt($cred)){
+            return response()->json([
+                'message'=>'Unauthorized.'
+            ], 401);
+        }
+        return $this->response(Auth::user());
+    }
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        
+        return response()->json([
+            'message'=>'You have successfully logged out.'
+        ]);
+    }
+}
